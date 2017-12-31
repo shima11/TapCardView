0dfbd6eab2bda92eafe105902df7a4abcd01e81b
shima <shima.jin@icloud.com>
2017-12-31 17:45:12 +0900

    Add new project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by jinsei shima on 2017/12/31.
+//  Copyright © 2017 jinsei shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
