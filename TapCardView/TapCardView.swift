ae7755f77bd0d164d4839d5cdf04d5266d195daf
Shima <shima.jin@icloud.com>
2020-07-18 11:53:06 +0900

    cleanup

--- TapCardView/TapCardView.swift
+++ TapCardView/TapCardView.swift
@@ -9,101 +9,104 @@
 import UIKit
 
 public enum TapPosition {
-
-    case left, right, bottom
+  
+  case left, right, bottom
 }
 
 public protocol CardViewDelegate: class {
-
-    func tapPosition(type: TapPosition, sender: TapCardView)
+  
+  func tapPosition(type: TapPosition, sender: TapCardView)
 }
 
 open class TapCardView: UIView {
-
-    public weak var delegate: CardViewDelegate?
-
-    // border of tap position. ratio of width and height. value is 0.0 to 1.0
-    public var horizontalBorder: CGFloat = 0.5
-    public var verticalBorder: CGFloat = 0.75
-
-    // flip animation settings
-    public var flipDegree: Float = 12
-    public var flipDuration: TimeInterval = 0.24
-
-    // MARK: Initialize
-
-    public override init(frame: CGRect) {
-
-        super.init(frame: frame)
-        setup()
+  
+  public weak var delegate: CardViewDelegate?
+  
+  // border of tap position. ratio of width and height. value is 0.0 to 1.0
+  public var horizontalBorder: CGFloat = 0.5
+  public var verticalBorder: CGFloat = 0.75
+  
+  // flip animation settings
+  public var flipDegree: Float = 12
+  public var flipDuration: TimeInterval = 0.24
+  
+  // MARK: Initialize
+  
+  public override init(frame: CGRect) {
+    
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  private func setup() {
+    
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGesture(sender:)))
+    addGestureRecognizer(tapGesture)
+    
+    layer.isDoubleSided = false
+  }
+  
+  // MARK: Public function
+  
+  @objc
+  open func tapGesture(sender: UITapGestureRecognizer) {
+    
+    if sender.state == .ended {
+      
+      let tapPoint = sender.location(in: self)
+      let tapPosition = getTapPosition(point: tapPoint, size: bounds.size)
+      delegate?.tapPosition(type: tapPosition, sender: self)
     }
-
-    required public init?(coder aDecoder: NSCoder) {
-
-        super.init(coder: aDecoder)
-        setup()
+  }
+  
+  public func getTapPosition(point: CGPoint, size: CGSize) -> TapPosition {
+    
+    if point.y >= size.height * verticalBorder {
+      return .bottom
     }
-
-    private func setup() {
-
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGesture(sender:)))
-        addGestureRecognizer(tapGesture)
-
-        layer.isDoubleSided = false
+    else if point.x >= size.width * horizontalBorder {
+      return .right
     }
-
-    // MARK: Public function
-
-    @objc
-    open func tapGesture(sender: UITapGestureRecognizer) {
-
-        if sender.state == .ended {
-
-            let tapPoint = sender.location(in: self)
-            let tapPosition = getTapPosition(point: tapPoint, size: bounds.size)
-            delegate?.tapPosition(type: tapPosition, sender: self)
-        }
+    else {
+      return .left
     }
-
-    public func getTapPosition(point: CGPoint, size: CGSize) -> TapPosition {
-
-        if point.y >= size.height * verticalBorder {
-            return .bottom
-        }
-        else if point.x >= size.width * horizontalBorder {
-            return .right
-        }
-        else {
-            return .left
-        }
+  }
+  
+  public func flipCard(type: TapPosition) {
+    
+    if type == .bottom { return }
+    
+    func degree2radian(d: Float) -> CGFloat {
+      
+      let r = Float.pi * d/180
+      return CGFloat(r)
     }
+    
+    // degree of rotation, when left end or right end.
+    let radius: Float = (type == .left) ? flipDegree : -flipDegree
+    let duration = flipDuration
 
-    public func flipCard(type: TapPosition) {
+    var transform = CATransform3DIdentity
+    transform.m34 = 1/1000
 
-        if type == .bottom { return }
-
-        // degree of rotation, when left end or right end.
-        let radius: Float = (type == .left) ? flipDegree : -flipDegree
-        let duration = flipDuration
-
-        UIView.animateKeyframes(withDuration: duration, delay: 0, options: [.allowUserInteraction], animations: {
-            var transform = CATransform3DIdentity
-            transform.m34 = 1/1000
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: duration/2, animations: {
-                self.layer.transform = CATransform3DRotate(transform, self.degree2radian(d: radius), 0, 1, 0)
-            })
-            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: duration/2, animations: {
-                self.layer.transform = CATransform3DRotate(transform, self.degree2radian(d: 0), 0, 1, 0)
-            })
-        }, completion: nil)
-    }
-
-    // MARK: Private function
-
-    private func degree2radian(d: Float) -> CGFloat {
-
-        let r = Float.pi * d/180
-        return CGFloat(r)
-    }
+    let rotation1 = CATransform3DRotate(transform, degree2radian(d: radius), 0, 1, 0)
+    let rotation2 = CATransform3DRotate(transform, degree2radian(d: 0), 0, 1, 0)
+    
+    UIView.animateKeyframes(withDuration: duration, delay: 0, options: [.allowUserInteraction], animations: {
+      UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: duration/2, animations: {
+        self.layer.transform = rotation1
+      })
+      UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: duration/2, animations: {
+        self.layer.transform = rotation2
+      })
+    }, completion: nil)
+  }
+  
 }
 
